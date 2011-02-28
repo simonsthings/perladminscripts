ef2ecf2050f1d052b95a4655975cdea715921c93
simon <simonsunimail@gmail.com>
2011-02-28 20:15:42 +0000

    made packWebView.pl usable. Further improvements possible... But first now back to matlab!
    git-svn-id: file:///Users/simon/Workspaces/WorkspaceSVN/ISIP_svn/svn/isipadminscripts/trunk@40 9ae2eace-b14e-0410-9d01-ee4776aa952a

--- runMatlabCodeOnRechenknecht/forClient/viewSimulationResults.pl
+++ runMatlabCodeOnRechenknecht/forClient/viewSimulationResults.pl
@@ -5,14 +5,31 @@ use strict;           # Make us need to declare each variable for easier error t
 #my $currDir = cwd;
 
 my $remotefolder = "/home/simon/Work/Projects/Simulations/remote";
-my $remotefilename = "controlscripts/checkSimulationStatus.pl";
-my $localfilepath = "/Users/simon/Documents/MATLAB/runMatlabCodeOnRechenknecht/forServer";
+my $remotefilename = "controlscripts/packWebView.pl";
+my $localServerscriptPath = "/Users/simon/Documents/MATLAB/runMatlabCodeOnRechenknecht/forServer";
+my $localResultsPath = "/Users/simon/Documents/MATLAB/rechenknechtResults";
+my $simtype = "singletrials";
 my $cmd;
 my @cmdoutput;
 
-#$cmd = "scp $localfilepath/$remotefilename nyquist.isip.uni-luebeck.de:$remotefolder/controlscripts/"; #print "$cmd\n";
+print "Packing & Fetching simulation results from server. \n";
+
+#$cmd = "scp $localServerscriptPath/$remotefilename nyquist.isip.uni-luebeck.de:$remotefolder/controlscripts/"; #print "$cmd\n";
 #@cmdoutput = `$cmd 2>&1`; print "@cmdoutput";
 
 $cmd = "ssh -n nyquist.isip.uni-luebeck.de \"cd $remotefolder ; ./$remotefilename @ARGV \""; #print "$cmd\n";
 @cmdoutput = `$cmd 2>&1`; print " @cmdoutput";
+if ($?) {die ("ERROR: The remote script execution failed! Please check what went wrong!\n");};
+
+$cmd = "scp nyquist.isip.uni-luebeck.de:$remotefolder/webview/${simtype}_webview.tgz $localResultsPath/"; #print "$cmd\n";
+@cmdoutput = `$cmd 2>&1`; print "@cmdoutput";
+
+$cmd = "tar xzf $localResultsPath/${simtype}_webview.tgz -C $localResultsPath/"; #print "$cmd\n";
+@cmdoutput = `$cmd 2>&1`; print "@cmdoutput";
+
+print "Opening results in web browser: $localResultsPath/webview/$simtype/index.html !";
+
+# Works on Mac (linux not tested):
+$cmd = "open $localResultsPath/webview/$simtype/index.html"; #print "$cmd\n";
+@cmdoutput = `$cmd 2>&1`; print "@cmdoutput";
 
--- runMatlabCodeOnRechenknecht/forServer/controlscripts/packWebView.pl
+++ runMatlabCodeOnRechenknecht/forServer/controlscripts/packWebView.pl
@@ -46,7 +46,7 @@ my @simstartdates;
 my @simstarttimes;
 my @simstatuses;
 #foreach my $subfolder (@subfolders)
-for (my $i = 0; $i < $#subfolders; $i++)
+for (my $i = 0; $i <= $#subfolders; $i++)
 {
     my $subfolder = $subfolders[$i];
     my $cmd2;
@@ -76,21 +76,21 @@ for (my $i = 0; $i < $#subfolders; $i++)
         #if ($?) {print "\n${cmdoutput3}\n"; print 'WARNING: It seems that the above folder does not contain a file called "screenerrors.txt". Old folder?';};
         if (substr($cmdoutput3,0,1) eq "0")
         {        
-	    $simstatus = "still running... "; 
+	    $simstatus = "still running... <br>"; 
 	
     	    my $cmd4 = "tail -n 3 ./$simtype/$subfolder/results/screenoutput.txt"; #print "${cmd4}\n";
     	    my @cmdoutput4 = "lala";
     	    @cmdoutput4 = `$cmd4 2>&1`;
     	    if ($?) {print "\n@{cmdoutput4}\n"; die 'ERROR: It seems that the above command has not worked! Read the screen output to find out why';};
     	    chomp(@cmdoutput4);
-    	    if ( (@cmdoutput4 ne "") && ($cmdoutput4[0] =~ m/((remaining): (.*))\s*/) ) { $simstatus .= "($1)"; }
+    	    if ( (@cmdoutput4 ne "") && ($cmdoutput4[0] =~ m/((remaining): (.*))\s*/) ) { $simstatus .= "($3 $2)"; }
     	    else {$simstatus .= "(no data on remaining time available yet)";}
         
     	    #print " @{cmdoutput4}";
 	}
 	else
 	{
-	    $simstatus = "error! (see ./$simtype/$subfolder/results/screenerrors.txt on server)";
+	    $simstatus = "error! <br> (see below)";
 	}
     }
     else
@@ -139,22 +139,28 @@ print SUMMARYFILE "<h1>Simulation Results</h1>\n";
 print SUMMARYFILE "<h2>Last $lastN $simtype:</h2>\n";
 #print OUTKFILE "<br>\n";
 
-print SUMMARYFILE "<table><tr>\n";
+print SUMMARYFILE "<table border=1 rules='all' frame='outer' cellspacing=0 cellpadding=0><tr>\n";
 # headers:
-for (my $i = 0; $i < $#subfolders; $i++)
+for (my $i = ($#subfolders); $i >= 0; $i--)
 {
-    print SUMMARYFILE "<th> $simstartdates[$i], $simstarttimes[$i] </th>\n";
+    print SUMMARYFILE "<th border=0> $simstartdates[$i] <br> $simstarttimes[$i] </th>\n";
 }
 print SUMMARYFILE "</tr><tr>\n";
-for (my $i = 0; $i < $#subfolders; $i++)
+for (my $i = ($#subfolders); $i >= 0; $i--)
 {
     print SUMMARYFILE "<th> $simstatuses[$i] </th>\n";
 }
 print SUMMARYFILE "</tr><tr>\n";
 # data:
-for (my $i = 0; $i < $#subfolders; $i++)
+for (my $i = ($#subfolders); $i >= 0; $i--)
 {
-    print SUMMARYFILE "<td> images... textfiles... </td>\n";
+    my $fullscreenlink = generateFullscreen(\@subfolders,$i,\@simstartdates,\@simstarttimes,\@simstatuses);
+    my $iframelink = generateiFrame(\@subfolders,$i);
+    
+    print SUMMARYFILE "<td>";
+    print SUMMARYFILE "<center><a href='$fullscreenlink'>View full screen...</a></center> ";
+    print SUMMARYFILE "<iframe src='$iframelink' frameborder=0 width=300 height=400 > no iframe support? </iframe>";
+    print SUMMARYFILE "</td>\n";
 }
 
 print SUMMARYFILE "</tr></table>\n\n";
@@ -175,6 +181,178 @@ $cmd = "tar czf ./webview/${simtype}_webview.tgz ./webview/$simtype"; #print "$c
 #print "@cmdoutput";
 
 print "Done.\n";
+
+
+## End of script! ##
+
+
+## Helper Functions: ##
+sub generateFullscreen
+{
+    my ($subfolders_ref,$i,$simstartdates_ref,$simstarttimes_ref,$simstatuses_ref) = @_;
+    my @subfolders = @$subfolders_ref;
+    my @simstartdates = @$simstartdates_ref;
+    my @simstarttimes = @$simstarttimes_ref;
+    my @simstatuses = @$simstatuses_ref;
+    
+    open(FULLSCREENFILE, "> ./webview/$simtype/$subfolders[$i]/fullscreen.html") or die "Can't write to file ./webview/$simtype/index.html: $!";
+
+    # Make HTML header:
+    print FULLSCREENFILE '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">', "\n";
+    print FULLSCREENFILE "<html><head><title>$simtype Simulation Results</title>";
+    #print OUTKFILE "<meta HTTP-EQUIV=\"REFRESH\" content=\"2; url=/$mainmenucache#$ENV{QUERY_STRING}\">";
+    print FULLSCREENFILE "</head><body link='#000000' vlink='#000000' alink='blue' bgcolor='#E0E0E0'>\n";
+    print FULLSCREENFILE "<font FACE='Helvetica, Arial, Verdana, Tahoma'>";
+    
+    print FULLSCREENFILE "<h1>Viewing trial: $simstartdates[$i], $simstarttimes[$i] </h1>";
+    print FULLSCREENFILE "<h3>Status: $simstatuses[$i]</h3>";
+    
+    
+    # HTML table
+    #print FULLSCREENFILE "<table border=0 ><tr><td valign='top'>";
+
+    ## Images:
+    print FULLSCREENFILE "<h2>Images:</h2>\n";    
+    $cmd = "cd ./webview/$simtype/$subfolders[$i]/; ls -1 *.png"; #print "$cmd\n";
+    my @imagefiles = `$cmd 2>&1`; 
+    if ($?) 
+    {
+	print FULLSCREENFILE "No images (yet)... <br><br>";
+	## just ignore if no images were found..
+	#print "\n$!"; 
+	#die 'ERROR: It seems that the above command has not worked! Read the screen output to find out why';
+    }
+    else
+    {
+	chomp(@imagefiles);
+	#print FULLSCREENFILE "@imagefiles";
+	foreach my $imagefile (@imagefiles)
+	{
+	    print FULLSCREENFILE "<a name='$imagefile'></a><img src='./$imagefile' > <br>";
+	    print FULLSCREENFILE "$imagefile <br><br>\n";
+	}
+    }
+
+    # HTML table
+    #print FULLSCREENFILE "</td><td valign='top'>";
+
+    ## Errors:
+    $cmd = "cat ./webview/$simtype/$subfolders[$i]/screenerrors.txt"; #print "$cmd\n";
+    my @anyerrors = `$cmd 2>&1`; if ($?) {print "\n@anyerrors\n"; die 'ERROR: It seems that the above command has not worked! Read the screen output to find out why';};
+    #print "@anyerrors";
+    print FULLSCREENFILE "<h2>Errors:</h2>";    
+    if ("@anyerrors" ne "")
+    {
+	print FULLSCREENFILE "<pre>@anyerrors</pre>"; 
+    }
+    else
+    {
+	print FULLSCREENFILE "No Errors (yet)...<br><br>";
+    }
+
+    ## Screen Outputs:
+    print FULLSCREENFILE "<h2>Screen Outputs:</h2>";
+    $cmd = "cat ./webview/$simtype/$subfolders[$i]/screenoutput.txt"; #print "$cmd\n";
+    @cmdoutput = `$cmd 2>&1`; if ($?) {print "\n@cmdoutput\n"; die 'ERROR: It seems that the above command has not worked! Read the screen output to find out why';};
+    print FULLSCREENFILE "<pre>@cmdoutput</pre>"; 
+
+    
+    # HTML table
+    #print FULLSCREENFILE "</td></tr></table>";
+    
+    close FULLSCREENFILE;
+    
+    return "./$subfolders[$i]/fullscreen.html";
+}
+
+sub generateiFrame
+{
+    my ($subfolders_ref,$i) = @_;
+    my @subfolders = @$subfolders_ref;
+    
+    open(IFRAMEFILE, "> ./webview/$simtype/$subfolders[$i]/iframe.html") or die "Can't write to file ./webview/$simtype/index.html: $!";
+
+    # Make HTML header:
+    print IFRAMEFILE '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">', "\n";
+    print IFRAMEFILE "<html><head><title>Simulation Results</title>";
+    #print OUTKFILE "<meta HTTP-EQUIV=\"REFRESH\" content=\"2; url=/$mainmenucache#$ENV{QUERY_STRING}\">";
+    print IFRAMEFILE "</head><body link='#000000' vlink='#000000' alink='blue' bgcolor='#E0E0E0'>\n";
+    print IFRAMEFILE "<font size=2>";# FACE='Helvetica, Arial, Verdana, Tahoma'>";
+    
+    #print IFRAMEFILE "i=$i,<br> $subfolders[$i]";
+    #print IFRAMEFILE "Status: lala";
+
+    # HTML table
+    #print IFRAMEFILE "<table border=0 ><tr><td valign='top'>";
+
+    ## Simulation Commands:
+    print IFRAMEFILE "<b>Simulation Command:</b><br>\n";
+    $cmd = "cat ./$simtype/$subfolders[$i]/codeandinputdata/remoteCommand.m"; #print "$cmd\n";
+    @cmdoutput = `$cmd 2>&1`; if ($?) {print "\n@cmdoutput\n"; die 'ERROR: It seems that the above command has not worked! Read the screen output to find out why';};
+    print IFRAMEFILE "<pre>@cmdoutput</pre>"; 
+
+
+    ## Images:
+    print IFRAMEFILE "<b>Images:</b><br>\n";    
+    $cmd = "cd ./webview/$simtype/$subfolders[$i]/; ls -1 *.png"; #print "$cmd\n";
+    my @imagefiles = `$cmd 2>&1`; 
+    if ($?) 
+    {
+	## just ignore if no images were found..
+	#print "\n$!"; 
+	#die 'ERROR: It seems that the above command has not worked! Read the screen output to find out why';
+	print IFRAMEFILE "No images (yet)...<br><br>";
+    }
+    else
+    {
+	chomp(@imagefiles);
+	#print IFRAMEFILE "@imagefiles";
+	foreach my $imagefile (@imagefiles)
+	{
+	    print IFRAMEFILE "<a href='fullscreen.html#$imagefile' target='_top'><img src='./$imagefile' width=250 ></a><br>";
+	    print IFRAMEFILE "$imagefile <br><br>\n";
+	}
+    }
+
+    # HTML table
+    #print IFRAMEFILE "</td><td valign='top'>";
+
+    ## Errors:
+    $cmd = "cat ./webview/$simtype/$subfolders[$i]/screenerrors.txt"; #print "$cmd\n";
+    my @anyerrors = `$cmd 2>&1`; if ($?) {print "\n@anyerrors\n"; die 'ERROR: It seems that the above command has not worked! Read the screen output to find out why';};
+    #print "@anyerrors";
+    print IFRAMEFILE "<b>Errors:</b><br>";    
+    if ("@anyerrors" ne "")
+    {
+	print IFRAMEFILE "<pre>@anyerrors</pre><br>"; 
+    }
+    else
+    {
+	print IFRAMEFILE "No Errors (yet)...<br><br>";
+    }
+
+    ## Screen Outputs:
+    print IFRAMEFILE "<b>Screen Outputs:</b><br>";
+    $cmd = "cat ./webview/$simtype/$subfolders[$i]/screenoutput.txt"; #print "$cmd\n";
+    @cmdoutput = `$cmd 2>&1`; if ($?) {print "\n@cmdoutput\n"; die 'ERROR: It seems that the above command has not worked! Read the screen output to find out why';};
+    print IFRAMEFILE "<pre>@cmdoutput</pre>"; 
+
+    ## Server & PID:
+    print IFRAMEFILE "<b>Server and PID:</b><br>\n";
+    $cmd = "cat ./$simtype/$subfolders[$i]/results/hostnamepid.txt"; #print "$cmd\n";
+    @cmdoutput = `$cmd 2>&1`; if ($?) {print "\n@cmdoutput\n"; die 'ERROR: It seems that the above command has not worked! Read the screen output to find out why';};
+    print IFRAMEFILE "<pre>@cmdoutput</pre>"; 
+    print "The results were stored in the file results.mat .";
+
+    
+    # HTML table
+    #print IFRAMEFILE "</td></tr></table>";
+    
+    
+    close IFRAMEFILE;
+    
+    return "./$subfolders[$i]/iframe.html";
+}
 
 
 
